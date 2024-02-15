9afb573423cfe4ba46dd80dae58363300a1577f1
another-phytophile <yujerry2002@gmail.com>
2024-02-15 17:11:28 -0500

    Finish the Preliminary Data Cleaning

--- exploratory analysis.R
+++ exploratory analysis.R
@@ -1,8 +1,55 @@
+# Purpose: Clean and Process Data for the Cuenca Orchid Project to a point where it can be used for an ANOVA. 
+
+# Library Statements
+library(here)
 library(readxl)
+library(tidyverse)
+library(compareDF)
 
-testorchids=read_excel("C:/Users/yujer/Downloads/Tabla_de_medidas.xlsx")
 
-testorchids
+# Import
+raworchids=read_excel("Data/Tabla_de_medidas.xlsx") %>% as_tibble()
 
-print(testorchids,n=50)
+help("read_excel")
+print(raworchids,n=30)
 
+# Data Cleaning
+
+#Create Simpler Names
+names(raworchids) <- c("es","med","p","nr","r","lr","l")
+
+# Assign Values to Merged Data
+orchids <-  raworchids %>% fill(es, med, p, nr, l) %>%
+  mutate(l = as.factor(l)) %>%
+  mutate(es = as.factor(es)) %>%
+  mutate(med = as.factor(med)) %>%
+  filter(!grepl("N", orchids$p) | is.na(p))
+
+# Average Root Numbers and Root Length
+
+# Check to see if any observations with 0 roots are incorrect
+ifelse(nrow((filter(orchids,nr==0 & r !=0)))==0, TRUE,FALSE)
+
+# Sum values
+orchidsum <-
+  orchids %>% 
+  group_by(es, med, l, p) %>% 
+  summarise(rnum = mean(lr),nr1=last(nr),nr2=n()) 
+
+# Get correct Root Number Values
+orchidsrn <- orchidsum %>% mutate(nr = ifelse(nr1 == 0, 0,
+                                              ifelse(nr1 >= nr2, nr1, nr2))) %>% 
+  ungroup()
+
+# Create Dummy/Assumed Data for 11, 0.5
+orchidsfinal <- orchidsrn %>% select(-nr2,-nr1) %>% 
+  add_row(es=c(rep(as.factor(11),14)),
+          med=c(rep(as.factor(0.5),14)),
+          l=c(rep(as.factor("Oscuridad"),7),
+              rep(as.factor("Luz"),7)),
+          p=c(as.character(1:14)),
+          rnum=0,
+          nr=0
+                                      )
+write.csv(orchidsfinal,"finaltest.csv")
+help("rep")
